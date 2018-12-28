7135334ceceaabf0166c11ab48d878acccf9ae17
brain-kracha <brian.kracha@gmail.com>
2018-12-28 15:30:50 -0700

    photos are viewable but not sixed properly yet

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react'
 
 const Gallery = ({url}) => {
   const styles = {
-    backgroundImage: `url(${url})`,
-    width:300,
-    height:350,
+    background: `url(${url})`,
+    width:'100%',
+    height:400,
     zIndex:10,
-    // backgroundSize: 'cover',
-    // backgroundPostion: 'center'
-    
+    backgroundSize: 'cover',
+    backgroundPostion: 'center'
   }
 
   return (
-    <div className="gallery" style={styles}></div>
+    <div className="gallery" style={styles}>
+    </div>
   )
 }
 
